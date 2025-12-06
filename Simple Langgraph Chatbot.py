cf8086380d8467ba2e787ededd6d5f46c302ec1f
Ali Khademi Khoubani <153095971+alikhademikhoubani@users.noreply.github.com>
2025-12-06 11:10:19 -0800

    Add files via upload

---
+++ Simple Langgraph Chatbot.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from typing import Annotated, Literal
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from pydantic import BaseModel, Field
+from typing_extensions import TypedDict
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+
+llm = ChatGroq(model_name = "llama-3.3-70b-versatile")
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+user_input = input("Enter a messages: ")
+state = graph.invoke({"messages": [{"role": "user", "content": user_input}]})
+
+print(state["messages"][-1].content)
